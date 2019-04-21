c7a7c77b2d061bd69af75cfa2b71a47523ab966a
Matthew Downey <matthewdowney20@gmail.com>
2019-04-21 12:23:43 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.mjdowney/schemata "1.0.0"
+  :description "A utility for abstracting away names and places for file-like data storage."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
