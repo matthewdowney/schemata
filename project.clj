a80a7d72db91fc967359281f78706e255375160c
Matthew Downey <matthewdowney20@gmail.com>
2019-04-21 12:30:01 -0500

    Clojure 10

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A utility for abstracting away names and places for file-like data storage."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]])
